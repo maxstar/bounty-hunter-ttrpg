0be3a49622b692fe03e62c89eebfbb30a66842d3
Maxter <maxim.starovoitov@gmail.com>
2021-03-22 02:22:59 +0200

    - cargo form - display cargo in starship

--- script/sheet/cargo.js
+++ script/sheet/cargo.js
@@ -3,14 +3,9 @@ import { BountyHunterItemSheet } from "./item.js";
 export class BountyHunterCargoSheet extends BountyHunterItemSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
+      classes: ["bounty-hunter", "sheet", "item", "cargo"],
       template: "systems/bounty-hunter-ttrpg/template/cargo.html",
-      tabs: [
-        {
-          navSelector: ".sheet-tabs",
-          contentSelector: ".sheet-body",
-          initial: "main",
-        },
-      ],
+      tabs: [],
     });
   }
 }
